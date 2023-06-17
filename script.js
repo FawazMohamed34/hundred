97ff5742a76040200af1514433149502d9678c46
mohamed <moefwzs@gmail.com>
2023-06-17 23:55:40 +0200

    ModifyAlertAndItsStyle

--- script.js
+++ script.js
@@ -17,6 +17,39 @@ let player2Title = document.getElementById('player2title'); // Title2 "Player "
   currentPlayer = 1,
   dice;
 
+let player1Name,
+    player2Name;
+
+  function getPlayerNames() {
+    let input1 = prompt("Nom du joueur 1 :");
+    let input2 = prompt("Nom du joueur 2 :");
+  
+  // Vérifier si les noms ont été entrés et ne sont pas vides
+  if (!input1 || input1.trim() === "") {
+    input1 = "Player 1"; // Utiliser une valeur par défaut si le nom n'est pas valide
+  }
+
+  if (!input2 || input2.trim() === "") {
+    input2 = "Player 2"; // Utiliser une valeur par défaut si le nom n'est pas valide
+  }
+
+  // Valider les noms en utilisant des critères spécifiques
+  const nameRegex =/^[A-z0-9\s]{1,30}$/; // Regex pour autoriser les lettres, chiffres et espaces et limiter à 30 caractères
+  if (!nameRegex.test(input1)) {
+    input1 = "Player 1"; // Utiliser une valeur par défaut si le nom ne respecte pas les critères
+  }
+
+  if (!nameRegex.test(input2)) {
+    input2 = "Player 2"; // Utiliser une valeur par défaut si le nom ne respecte pas les critères
+  }
+    player1Name = input1.trim();
+    player2Name = input2.trim();
+    player1Title.textContent = player1Name;
+    player2Title.textContent = player2Name;
+  }
+
+
+getPlayerNames();
 
 // Restore all, it's a new game!
 const restore = () => {
@@ -24,6 +57,7 @@ const restore = () => {
   elGlobalScorePlayer2.textContent ='0';  
   elCurrentScorePlayer1.textContent ='0';
   elCurrentScorePlayer2.textContent = '0';
+  getPlayerNames();
 };
 
 const changeCurrentPlayer = () => {
@@ -63,9 +97,15 @@ const holdHandler = () => {
   currentPlayer === 1 ? (elGlobalScorePlayer1.textContent = globalScorePlayer1) : (elGlobalScorePlayer2.textContent = globalScorePlayer2);
   
   if (globalScorePlayer1 >= 100 || globalScorePlayer2 >= 100) {
-    // If player1 or player2 has reached 100, it's finish, restore for a new game
-    alert(`Player ${currentPlayer} a gagné!`);
-    restore();
+    let winnerName = currentPlayer === 1 ? player1Name : player2Name;
+    const alertContent = document.createElement('div');
+    alertContent.classList.add('alert', 'alert-success');
+    alertContent.textContent = `(${winnerName}) a gagné!`;
+    // Ajouter l'alerte au corps du document
+    document.body.appendChild(alertContent);
+    setTimeout(() => {
+      alertContent.remove();
+    }, 6000);
   } else {
     changeCurrentPlayer();
   }
